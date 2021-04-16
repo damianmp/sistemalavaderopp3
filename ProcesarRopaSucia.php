d40de7a0a7704f44debe31da0ffe71db90261f84
damian elias <33334823+damianmp@users.noreply.github.com>
2021-04-16 17:58:46 -0300

    Add files via upload

---
+++ ProcesarRopaSucia.php
@@ -0,0 +1,17 @@
+<?php
+    include_once("html/Header.php");
+    session_start();
+    if(isset($_SESSION['usuario'])){
+         $usuDTOLogin = $_SESSION['usuario'];
+         foreach ($usuDTOLogin->getM_rol() as $rol){
+                if($rol->getM_id() == 2){
+                    //agregar movimiento a los pendientes para su devolucion
+                    MovimientoDAO::addMovimento($usuDTOLogin, $_SESSION['total'], $_SESSION['id_mov']);
+                    unset($_SESSION['id_mov']);
+                    echo "Espere por favor...";
+                    echo "<meta http-equiv=\"refresh\" content=\"2;url=https://www.sistemalavaderopp3.ml/\"/>";
+                }
+         }
+    }
+    include_once("html/Footer.php");
+?>
